bb3acef4d19d65f613d982a80dd0b87819ae2dc1
Gideon Isa <innocentgideon10@gmail.com>
2025-04-17 15:39:16 +0100

    added movie validation

---
+++ Movies.Application/Validators/MovieValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Movies.Application.Models;
+using Movies.Application.Respository;
+using Movies.Application.Services;
+
+namespace Movies.Application.Validators;
+
+public class MovieValidator : AbstractValidator<Movie>
+{
+    private readonly IMovieRepository _repository;
+    
+    public MovieValidator(IMovieRepository repository)
+    {
+        _repository = repository;
+        
+        RuleFor(m => m.Id)
+            .NotEmpty();
+
+        RuleFor(m => m.Genres)
+            .NotEmpty();
+
+        RuleFor(m => m.Title)
+            .NotEmpty();
+        
+        RuleFor(m => m.YearOfRelease)
+            .LessThanOrEqualTo(DateTime.UtcNow.Year);
+
+        RuleFor(m => m.Slug)
+            .MustAsync(ValidateSlug)
+            .WithMessage("This movie already exists in the system.");
+    }
+
+    private async Task<bool> ValidateSlug(Movie movie, string slug, CancellationToken cancellationToken = default)
+    {
+        var existingMovie = await _repository.GetBySlugAsync(slug);
+        if (existingMovie is not null)
+        {
+            return existingMovie!.Id == movie.Id;
+        }
+
+        return existingMovie is null;
+    }
+}
